52fe4d7fdf4276188564f32fb95d7f587b797bfb
Mike Goldenberg <csrutil@tuta.io>
2023-11-15 21:43:05 +0100

    adding shared ui components and layout

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
