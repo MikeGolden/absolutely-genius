2b8d0e4d7ead6fb0f54e15aef735c5a3131e73f0
Mike Goldenberg <csrutil@tuta.io>
2023-11-20 11:56:05 +0100

    form ui logic

---
+++ src/components/forms/AccountProfile.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import * as z from "zod";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { usePathname, useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Form } from "@/components/ui/form";
