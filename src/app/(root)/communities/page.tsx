d2d14d61d9444ce570851cb26aa3191d7bc11d04
Mike Goldenberg <csrutil@tuta.io>
2023-11-20 20:37:00 +0100

    Community Card

---
+++ src/app/(root)/communities/page.tsx
@@ -0,0 +1,5 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
